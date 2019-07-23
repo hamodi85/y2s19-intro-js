2a60ae1079280d4eaa62755941ad3dc23c2df8d1
myname21-meet <myname21@meet.mit.edu>
2019-07-23 11:27:41 -0400

    aaaa

---
+++ whatver.js
@@ -0,0 +1,13 @@
+var birthYear = parseInt(prompt("on what year were yu born?"));
+
+alert (birthYear);
+
+var luckyNumber = parseInt(prompt("what is your lucky number?"));
+
+alert (luckyNumber);
+
+
+var favFruit = mangos;
+var favSubject = math;
+
+console.log(birth)
